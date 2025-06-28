4efbc7be773d22e7f6941e28704c419983ffaf67
kwakseobang <iii6602@naver.com>
2025-06-28 20:03:30 +0900

    프로젝트 초기화

---
+++ src/main/java/org/kwakmunsu/npulsone/NpulsoneApplication.java
@@ -0,0 +1,13 @@
+package org.kwakmunsu.npulsone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpulsoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NpulsoneApplication.class, args);
+    }
+
+}
